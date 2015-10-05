dd8a57e18796a706149260eafc7653e43fe66169
Tobias Oetzel <tobias.oetzel@sap.com>
2015-10-05 14:23:50 +0200

    step nr. 10

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,17 +1,17 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+	"sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.demo.wt.Component", {
 
 		metadata : {
-			rootView: "sap.ui.demo.wt.view.App"
+			manifest: "json"
 		},
 
 		init : function () {
+
 			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
 
@@ -23,12 +23,6 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
-
-			// set i18n model
-			var i18nModel = new ResourceModel({
-				bundleName : "sap.ui.demo.wt.i18n.i18n"
-			});
-			this.setModel(i18nModel, "i18n");
 		}
 	});
 
